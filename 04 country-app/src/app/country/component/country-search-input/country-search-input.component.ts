4c7c246707892c30413700bf9b80d1e0276d9b05
Juan Tomas <juan.tomas88@hotmail.com>
2025-07-08 15:53:32 +0200

    servicio para buscar por capital

--- 04 country-app/src/app/country/component/country-search-input/country-search-input.component.ts
+++ 04 country-app/src/app/country/component/country-search-input/country-search-input.component.ts
@@ -1,10 +1,12 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  inject,
   input,
   output,
   signal,
 } from '@angular/core';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'country-search-input',
@@ -13,13 +15,16 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CountrySearchInputComponent {
+  countryService = inject(CountryService);
+
+  //Emite un valor hacia el componente padre (by-capital-page)
   value = output<string>();
 
   // placeHolderRecibido es un input porque está esperando un string (por defecto con la palabra buscar). ESte placeHolderRecibido
   // es el que irá a la derecha del igual para que la etiqueta del placeholder del input muestre su contenido
   placeholderRecibido = input<string>('Buscar');
 
-  onSearch(value: string) {
+  emitirBusqueda(value: string) {
     const busqueda: string = '';
     this.value.emit(busqueda);
     console.log(value);
--- 04 country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ 04 country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,10 +1,20 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import { CountrySearchInputComponent } from '../../component/country-search-input/country-search-input.component';
 import { CountryListComponent } from '../../component/country-list/country-list.component';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-by-capital-page',
   imports: [CountrySearchInputComponent, CountryListComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent {}
+export class ByCapitalPageComponent {
+  countryService = inject(CountryService);
+
+  buscarPorCapital(query: string) {
+    console.log({ query });
+    this.countryService.searchByCapital(query).subscribe((respuesta) => {
+      console.log(respuesta);
+    });
+  }
+}
